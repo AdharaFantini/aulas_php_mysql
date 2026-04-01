19a1844c124cd0115313f6c569e56ac9517cdece
AdharaFantini <2info.adhara@gmail.com>
2026-04-01 15:48:49 -0300

    sadasd

--- exercicios-condicionais-loops.php
+++ exercicios-condicionais-loops.php
@@ -13,11 +13,45 @@ echo "Nome do cliente: $nome";
 echo "<br>------------------------------------------------------------------------------------------------<br>";
 
 $saldoInicial = 100;
+$deposito = 20;
+
 if ($saldoInicial > 0) {
     echo "Saldo inicial:" . $saldoInicial;}
 echo "<br>------------------------------------------------------------------------------------------------<br>";
 
-echo "Digite 1 para fazer depósito, 2 para fazer saque, 3 para empréstimo parcelado, 4 para ver o saldo atual";
+echo "Digite 1 para fazer depósito, 2 para fazer saque, 3 para empréstimo parcelado, 4 para ver o saldo atual . <br>";
+
+$escolha = 3;
+
+if ($escolha == 1) {
+    $deposito = 20;
+        $saldoFinal = $saldoInicial - $deposito;
+
+        if ($deposito < $saldoInicial) {
+    echo "Saldo pós depósito: " . $saldoFinal;}
+        else {
+            echo "Depósito superior ao Saldo. Não foi possível realizar o pagamento.";
+        }
+}
+
+if ($escolha == 2) {
+    $saque = 20;
+    $saldoSaque = $saldoInicial + $saque;
+        echo "Saldo pós saque: " . $saldoSaque;}
+
+if ($escolha == 3) {
+    $capital = 30;
+    $anos = 4;
+    $juros = $capital * 0.20 * $anos;
+    $totalpagar = $capital + $juros;
+    $saldoFinal = $saldoInicial - $totalpagar;
+    echo "Saldo após juros: " . $saldoFinal;}
+
+if ($escolha == 4) {
+    echo "Saldo atual: " . $saldoInicial;}
+
+elseif ($escolha >= 5){
+    echo "Agradecemos pela preferência.";}
 
 
 ?>
