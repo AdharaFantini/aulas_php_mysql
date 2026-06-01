9afcb89b8c0f3b1de2a3e453245a248f066e23c3
AdharaFantini <2info.adhara@gmail.com>
2026-06-01 11:06:05 -0300

    pineapple pne

--- arrays.aula.php
+++ arrays.aula.php
@@ -20,3 +20,22 @@ $danganrompa["Louco"] = "Nagito";
 $danganrompa["Mentiroso"] = "Kokishi";
 $danganrompa["Sádico"] = "Monokuma";
 var_dump($danganrompa);
+
+echo "<br><br>Arrays indexados: <br>";
+$carros = array("Volvo", "BMW", "Toyota");
+echo "<br>";
+var_dump($carros);
+echo "<br><br>";
+echo "Acessando item do array: <br>";
+echo "Posição" [0];
+
+echo "Arrays Associativas: <br>";
+$epic = array("Nome" => "Odisseu", "Anos" => 7, "Cargo" => "Rei de Ítaca");
+var_dump($epic);
+
+echo "<br><br>";
+echo "Acessando item do array: <br>";
+echo $epic["Nome"];
+
+echo "<br>Trocando item de uma array<br>";
+$epic["Anos"] = 20;
